40fd62848050b32d68cba10eb41052277d4db245
xsw <xsw006007@gmail.com>
2022-06-29 11:50:34 +0800

    Added next page link

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php require_once('Connections/SQLmy.php'); ?>
 <?php
+$currentPage = $_SERVER["PHP_SELF"];
+
 $maxRows_IndexRecordset = 3;
 $pageNum_IndexRecordset = 0;
 if (isset($_GET['pageNum_IndexRecordset'])) {
@@ -20,6 +22,22 @@ if (isset($_GET['totalRows_IndexRecordset'])) {
   $totalRows_IndexRecordset = mysql_num_rows($all_IndexRecordset);
 }
 $totalPages_IndexRecordset = ceil($totalRows_IndexRecordset/$maxRows_IndexRecordset)-1;
+
+$queryString_IndexRecordset = "";
+if (!empty($_SERVER['QUERY_STRING'])) {
+  $params = explode("&", $_SERVER['QUERY_STRING']);
+  $newParams = array();
+  foreach ($params as $param) {
+    if (stristr($param, "pageNum_IndexRecordset") == false && 
+        stristr($param, "totalRows_IndexRecordset") == false) {
+      array_push($newParams, $param);
+    }
+  }
+  if (count($newParams) != 0) {
+    $queryString_IndexRecordset = "&" . htmlentities(implode("&", $newParams));
+  }
+}
+$queryString_IndexRecordset = sprintf("&totalRows_IndexRecordset=%d%s", $totalRows_IndexRecordset, $queryString_IndexRecordset);
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -50,6 +68,17 @@ $totalPages_IndexRecordset = ceil($totalRows_IndexRecordset/$maxRows_IndexRecord
     <td><?php echo $row_IndexRecordset['Addr']; ?></td>
   </tr>
   <?php } while ($row_IndexRecordset = mysql_fetch_assoc($IndexRecordset)); ?>
+  
+  <tr>
+    <td><a href="<?php printf("%s?pageNum_IndexRecordset=%d%s", $currentPage, 0, $queryString_IndexRecordset); ?>">第一頁</a></td>
+    <td>
+	  <a href="<?php printf("%s?pageNum_IndexRecordset=%d%s", $currentPage, max(0, $pageNum_IndexRecordset - 1), $queryString_IndexRecordset); ?>">上一頁</a>
+	</td>
+    <td>
+	  <a href="<?php printf("%s?pageNum_IndexRecordset=%d%s", $currentPage, min($totalPages_IndexRecordset, $pageNum_IndexRecordset + 1),                   $queryString_IndexRecordset); ?>">下一頁</a>
+	</td>
+    <td><a href="<?php printf("%s?pageNum_IndexRecordset=%d%s", $currentPage, $totalPages_IndexRecordset, $queryString_IndexRecordset); ?>">最後頁</a></td>
+  </tr>
 </table>
 
 </body>
