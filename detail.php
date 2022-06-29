a85593362654bb37764f596d446721d019922a7a
xsw <xsw006007@gmail.com>
2022-06-29 13:47:32 +0800

    detail file layouted

--- detail.php
+++ detail.php
@@ -4,7 +4,26 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>無標題文件</title>
 </head>
-detail
+
 <body>
+<table width="100" border="1" cellspacing="1" cellpadding="1" align="center" style="margin-top:58px">
+  <tr>
+    <td>ID</td>
+    <td>Name</td>
+    <td>Old</td>
+    <td>Addr</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td colspan="2"><input type="submit" value="更新" /></td>
+    <td colspan="2">刪除</td>
+  </tr>
+</table>
+
 </body>
 </html>
