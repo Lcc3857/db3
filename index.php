5711996dcc0079b3a2246471ce46ee32f7dfc817
xsw <xsw006007@gmail.com>
2022-06-29 11:41:48 +0800

    show row of loop complete

--- index.php
+++ index.php
@@ -1,10 +1,25 @@
 <?php require_once('Connections/SQLmy.php'); ?>
 <?php
+$maxRows_IndexRecordset = 3;
+$pageNum_IndexRecordset = 0;
+if (isset($_GET['pageNum_IndexRecordset'])) {
+  $pageNum_IndexRecordset = $_GET['pageNum_IndexRecordset'];
+}
+$startRow_IndexRecordset = $pageNum_IndexRecordset * $maxRows_IndexRecordset;
+
 mysql_select_db($database_SQLmy, $SQLmy);
 $query_IndexRecordset = "SELECT * FROM db3";
-$IndexRecordset = mysql_query($query_IndexRecordset, $SQLmy) or die(mysql_error());
+$query_limit_IndexRecordset = sprintf("%s LIMIT %d, %d", $query_IndexRecordset, $startRow_IndexRecordset, $maxRows_IndexRecordset);
+$IndexRecordset = mysql_query($query_limit_IndexRecordset, $SQLmy) or die(mysql_error());
 $row_IndexRecordset = mysql_fetch_assoc($IndexRecordset);
-$totalRows_IndexRecordset = mysql_num_rows($IndexRecordset);
+
+if (isset($_GET['totalRows_IndexRecordset'])) {
+  $totalRows_IndexRecordset = $_GET['totalRows_IndexRecordset'];
+} else {
+  $all_IndexRecordset = mysql_query($query_IndexRecordset);
+  $totalRows_IndexRecordset = mysql_num_rows($all_IndexRecordset);
+}
+$totalPages_IndexRecordset = ceil($totalRows_IndexRecordset/$maxRows_IndexRecordset)-1;
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -26,12 +41,15 @@ $totalRows_IndexRecordset = mysql_num_rows($IndexRecordset);
     <td>Old</td>
     <td>Addr</td>
   </tr>
+  
+  <?php do { ?>
   <tr>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
+    <td><?php echo $row_IndexRecordset['ID']; ?></td>
+    <td><?php echo $row_IndexRecordset['Name']; ?></td>
+    <td><?php echo $row_IndexRecordset['Old']; ?></td>
+    <td><?php echo $row_IndexRecordset['Addr']; ?></td>
   </tr>
+  <?php } while ($row_IndexRecordset = mysql_fetch_assoc($IndexRecordset)); ?>
 </table>
 
 </body>
