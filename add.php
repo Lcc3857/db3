70e978f1c9dba2c1dff81750e09833610e605863
xsw <xsw006007@gmail.com>
2022-06-29 11:19:59 +0800

    Layout complete

--- add.php
+++ add.php
@@ -6,5 +6,24 @@
 </head>
 
 <body>
+
+<form name="form1">
+<table width="100" border="1" cellspacing="1" cellpadding="1" align="center" style="margin-top:58px">
+  <tr>
+    <td>Name</td>
+    <td>Old</td>
+    <td>Addr</td>
+  </tr>
+  <tr>
+    <td><input type="text" name="Name" /></td>
+    <td><input type="text" name="Old" /></td>
+    <td><input type="text" name="Addr" /></td>
+  </tr>
+  <tr>
+    <td colspan="3" align="right"><input type="submit" value="新增" /></td>
+  </tr>
+</table>
+</form>
+
 </body>
 </html>
