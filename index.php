ddb037523f394e603d1b8bd6544cd8e6f622b05f
xsw <xsw006007@gmail.com>
2022-06-29 11:30:20 +0800

    Layout index complete

--- index.php
+++ index.php
@@ -6,5 +6,26 @@
 </head>
 
 <body>
+<table border="1" cellspacing="1" cellpadding="1" align="center" style="margin-top: 58px">
+  <tr>
+    <td colspan="4" align="right">新增</td>
+  </tr>
+  <tr>
+    <td colspan="4">從x~y共z筆</td>
+  </tr>
+  <tr>
+    <td>ID</td>
+    <td>Name</td>
+    <td>Old</td>
+    <td>Addr</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+
 </body>
 </html>
