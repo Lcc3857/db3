77514765402cbf925d27da7c8033023cb66d3043
xsw <xsw006007@gmail.com>
2022-06-29 16:17:35 +0800

    updtae of detail complete

--- detail.php
+++ detail.php
@@ -1,5 +1,53 @@
 <?php require_once('Connections/SQLmy.php'); ?>
 <?php
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  $theValue = (!get_magic_quotes_gpc()) ? addslashes($theValue) : $theValue;
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+
+$editFormAction = $_SERVER['PHP_SELF'];
+if (isset($_SERVER['QUERY_STRING'])) {
+  $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
+}
+
+if ((isset($_POST["MM_update"])) && ($_POST["MM_update"] == "form1")) {
+  $updateSQL = sprintf("UPDATE db3 SET Name=%s, `Old`=%s, Addr=%s WHERE ID=%s",
+                       GetSQLValueString($_POST['Name'], "text"),
+                       GetSQLValueString($_POST['Old'], "int"),
+                       GetSQLValueString($_POST['Addr'], "text"),
+                       GetSQLValueString($_POST['ID'], "int"));
+
+  mysql_select_db($database_SQLmy, $SQLmy);
+  $Result1 = mysql_query($updateSQL, $SQLmy) or die(mysql_error());
+
+  $updateGoTo = "index.php";
+  if (isset($_SERVER['QUERY_STRING'])) {
+    $updateGoTo .= (strpos($updateGoTo, '?')) ? "&" : "?";
+    $updateGoTo .= $_SERVER['QUERY_STRING'];
+  }
+  header(sprintf("Location: %s", $updateGoTo));
+}
+
 $colname_detailRecordset = "-1";
 if (isset($_GET['id'])) {
   $colname_detailRecordset = (get_magic_quotes_gpc()) ? $_GET['id'] : addslashes($_GET['id']);
@@ -17,13 +65,13 @@ $totalRows_detailRecordset = mysql_num_rows($detailRecordset);
 </head>
 
 <body>
-<form name="form1">
+<form method="POST" action="<?php echo $editFormAction; ?>" name="form1">
 <table width="100" border="1" cellspacing="1" cellpadding="1" align="center">
   <tr>
     <td>ID</td>
     <td>Name</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
+    <td>Old</td>
+    <td>Addr</td>
   </tr>
   <tr>
     <td><input type="text" name="ID" value="<?php echo $row_detailRecordset['ID']; ?>" readonly="true"/></td>
@@ -32,10 +80,11 @@ $totalRows_detailRecordset = mysql_num_rows($detailRecordset);
     <td><input type="text" name="Addr" value="<?php echo $row_detailRecordset['Addr']; ?>" /></td>
   </tr>
   <tr>
-    <td><input type="submit" name="" value="更新" /></td>
-    <td>刪除</td>
+    <td colspan="2"><input type="submit" name="" value="更新" /></td>
+    <td colspan="2">刪除</td>
   </tr>
 </table>
+<input type="hidden" name="MM_update" value="form1">
 </form>
 
 </body>
